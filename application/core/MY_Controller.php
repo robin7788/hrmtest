adf5f79373a8504386ade91c59a03971dc63f498
tara gurung <taraprasad336@gmail.com>
2015-09-10 16:49:18 -0700

    skeletion with ion oath and hmvc

---
+++ application/core/MY_Controller.php
@@ -0,0 +1,18 @@
+<?php (defined('BASEPATH')) OR exit('No direct script access allowed');
+
+/**
+ * Description of MY_Controller
+ * @author dnunez
+ */
+
+class MY_Controller extends MX_Controller
+{
+
+    public $data = array();
+
+    function __construct()
+    {
+        parent::__construct();
+    }
+
+}
